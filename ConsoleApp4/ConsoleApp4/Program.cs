5982fe5c6a29894933c5ff099b80dcbd23400793
AlanSholdanbayev <alannabiev15@gmail.com>
2024-09-20 23:30:10 +0500

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public interface INotificationService
+{
+    void Send(string message);
+}
+
+public class EmailService : INotificationService
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Отправка Email: {message}");
+    }
+}
+
+public class SmsService : INotificationService
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Отправка SMS: {message}");
+    }
+}
+
+public class Notification
+{
+    private readonly INotificationService _notificationService;
+    
+    public Notification(INotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    public void Send(string message)
+    {
+        _notificationService.Send(message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        INotificationService emailService = new EmailService();
+        Notification notification1 = new Notification(emailService);
+        notification1.Send("Ваш заказ подтвержден.");
+        
+        INotificationService smsService = new SmsService();
+        Notification notification2 = new Notification(smsService);
+        notification2.Send("Ваш заказ отправлен.");
+    }
+}
